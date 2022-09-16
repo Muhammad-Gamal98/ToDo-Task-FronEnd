4c5fc5c84dd66e4cbe4ee6c7a224d26d4ba500b1
Muhammad Gamal <moga1478@gmail.com>
2022-09-16 02:00:38 +0200

    Create layout and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Layout from "./components/layouts/Layout";
 import NotFound from "./pages/NotFound/NotFound";
+import "./assets/styles/styles.css";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "./components/Forms/LoginForm";
+import ForgetPassword from "./pages/ForgetPassword";
+import SignUp from "./components/Forms/SignUp";
 
 function App() {
   return (
     <>
-      <NotFound />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/forget-Password" element={<ForgetPassword />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
