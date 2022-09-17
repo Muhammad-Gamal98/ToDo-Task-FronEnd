77d8ac7419a195d36842ea6b6b2b5579ae3eb62a
Muhammad Gamal <moga1478@gmail.com>
2022-09-17 14:31:58 +0200

    ad Cookie Provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 import "./index.css";
 import App from "./App";
 import AuthProvider from "./store/auth-provider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AuthProvider>
+  <CookiesProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </CookiesProvider>
 );
